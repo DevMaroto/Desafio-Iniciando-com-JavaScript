4484881717e7b7434f9704785413e1427c69c94a
Pedro Teodoro <pedropauloteodoro26@gmail.com>
2023-07-04 06:47:58 -0300

    Challenge completed

---
+++ main.js
@@ -0,0 +1,82 @@
+//Exercicio 1
+// alert("Hello World!")
+
+
+//Exercicio 2
+// let numero1 = Number(prompt("Digite o primero numero"))
+// let numero2 = Number(prompt("Digite o segundo numero"))
+// let soma = numero1 + numero2;
+// alert(`O resultado da soma é ${soma}`)
+
+//Exercicio 3
+// let numero = Number(prompt("Digite apenas numeros"))
+// if(typeof numero == "number" &&  !isNaN(numero)) {
+//   alert("É um número")
+// } else {
+//   alert("Não é um número")
+// }
+
+//Exercicio 4
+// let caracter = "conjunto de caracter"
+// if(typeof caracter == "string") {
+//   alert("É uma string")
+// } else {
+//   alert("Não é uma string")
+// }
+
+
+//Exercicio 5
+// let bool = true
+// if(typeof bool == "boolean"){
+//   alert("É um boolean")
+// } else{
+//   alert("Não é um boolean")
+// }
+
+//Exercicio 6
+// let n1 = Number(prompt("Digite o primeiro numero"))
+// let n2 = Number(prompt("Digite o segundo numero"))
+
+// let resultSub = n1 - n2
+
+// alert(`O resultado da subtração é ${resultSub}`)
+
+//Exercicio 7
+// let n1 = Number(prompt("Digite o primeiro"))
+// let n2 = Number(prompt("Digite o segundo numero"))
+
+// let resultMult = n1 * n2
+
+// alert(`O resultado da multiplicação é ${resultMult}`)
+
+//Exercicio 8
+// let n1 = Number(prompt("Digite o primeiro"))
+// let n2 = Number(prompt("Digite o segundo numero"))
+
+// let resultDiv = n1 / n2
+
+// alert(`O resultado da multiplicação é ${resultDiv}`)
+
+//Exercicio 9
+// let valor = Number(prompt("Digite um valor"))
+
+// let modulo = valor % 2
+// console.log(`modulo ${modulo}`)
+
+// if(modulo == 0){
+//   alert("O numero é par")
+// } else {
+//   alert("O numero não é par")
+// }
+
+//Exercicio 10
+let valor = Number(prompt("Digite um valor"))
+
+let modulo = valor % 2
+console.log(`modulo ${modulo}`)
+
+if(modulo != 0){
+  alert("O numero é impar")
+} else {
+  alert("O numero não é impar")
+}
